1289da4aedf716fffdfdf3f0b7e48619c6848522
Vytautas Grizas <vytautasgrizas@Vytautass-MacBook-Pro.local>
2018-10-22 01:21:32 +0300

    Updates

---
+++ precache-manifest.b1293e0b54884cb134e188f1ecf13795.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f2cf7a6b6c7098873f7ed572c070781",
+    "url": "/travelbro/static/media/logo.0f2cf7a6.svg"
+  },
+  {
+    "revision": "55cfe41e72334f239127",
+    "url": "/travelbro/static/js/runtime~main.55cfe41e.js"
+  },
+  {
+    "revision": "c2668dd8154bb2235275",
+    "url": "/travelbro/static/js/main.c2668dd8.chunk.js"
+  },
+  {
+    "revision": "729e267b35492fe3a310",
+    "url": "/travelbro/static/js/1.729e267b.chunk.js"
+  },
+  {
+    "revision": "c2668dd8154bb2235275",
+    "url": "/travelbro/static/css/main.a72e1c59.chunk.css"
+  },
+  {
+    "revision": "596c21e91c7131c5c87d907e8a4c11e8",
+    "url": "/travelbro/index.html"
+  }
+];
